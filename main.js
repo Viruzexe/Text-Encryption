213eac8d4ddbdbd88312e4483b5f22069dc0f720
David Leonardo Guerra Leon <dguerra@agrosavia.co>
2023-04-20 10:54:17 -0500

    New Commands

--- main.js
+++ main.js
@@ -107,27 +107,24 @@ let llaves = {
   }
   
   //creando el boton pegar aprendiendo el evento click Obtenemos el botón de pegar usando el método getElementById
-  const pegar = document.getElementById("pegar");
-  
-  //Asignamos un evento click al botón de pegar
-  pegar.addEventListener("click", function(event) {
-    event.preventDefault();
-    //Usamos el objeto navigator.clipboard para acceder al portapapeles
-    navigator.clipboard.readText()
-      .then((texto) => {
-        //Asignamos el texto del portapapeles al valor de la textarea que queramos
-        document.getElementById("textoOriginal").value = texto;
-        //Llamamos a la función mostrarOcultarDivs para activar la segunda textarea
-        mostrarOcultarDivs();
-        //Mostramos una notificación indicando que se ha pegado el texto
-        mostrarNotificacion("Texto pegado correctamente");
-      })
-      .catch((error) => {
-        //Mostramos una notificación indicando que hubo un error al pegar el texto
+  const pegar = document.querySelector("#pegar");
+
+  pegar.addEventListener("click", async function(event) {
+    try {
+      event.preventDefault();
+      const texto = await navigator.clipboard.readText();
+      const textarea = document.querySelector("#textoOriginal");
+      textarea.value = texto;
+      mostrarOcultarDivs();
+      mostrarNotificacion("Texto pegado correctamente");
+    } catch(error) {
+      if (error instanceof SecurityError) {
+        mostrarNotificacion("El usuario no dio permisos para acceder al portapapeles");
+      } else {
         mostrarNotificacion("Error al pegar el texto: " + error);
-      });
-  })
-  
+      }
+    }
+  });
   //Creamos la funcion restablecer para limpiar nuestra area de trabajo con el Boton restablecer
   function borrarSegundoTextarea() {
     let salida = document.getElementById("textoEncriptado");
